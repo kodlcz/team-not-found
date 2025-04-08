a7ba42774aa5683c15704570d6d568f75041e30d
Adam Kadlec <xkadlea00@fit.vut.cz>
2025-04-08 14:50:52 +0200

    vytvoření funkce add(sčítání) a sub(odčítání)

---
+++ repo/src/calc_lib.py
@@ -0,0 +1,22 @@
+"""
+@brief sčítá 2 čísla.
+    
+@param num1 první číslo (int or float).
+@param num2 druhé číslo (int or float).
+@return vrátí součet dvou čísel(int or float).
+    
+Tato funkce sčítá druhé číslo s prvním.
+"""
+def add(num1, num2):
+    return num1 + num2
+"""
+@brief odečítá 2 čísla.
+    
+@param num1 první číslo (int or float).
+@param num2 druhé číslo (int or float).
+@return vrátí rozdíl dvou čísel(int or float).
+    
+Tato funkce odečítá druhé číslo od prvního.
+"""
+def sub(num1, num2):
+    return num1 - num2
