117ed7cb4660b2882d783314bf9561ec8d3b27d1
Adam Kadlec <xkadlea00@fit.vut.cz>
2025-04-22 16:52:54 +0200

    dodelani prvniho nastrelu profilingu bez komentaru

--- repo/src/profiling.py
+++ repo/src/profiling.py
@@ -1,13 +1,18 @@
 from calc_lib import *
 import sys
-num = []
+nums = []
 numCount = 0
 count = 0
 for val in sys.stdin.read().split():
     try:
-        num.append(float(val))
-        numCount += val
-        count += 1
+        nums.append(float(val))
+        numCount = add(numCount,val)
+        count = add(count,1)
     except ValueError:
         print("zadané špatné číslo: ", val)
+prum = div(numCount,count)
+rozdil = 0
+for i in nums:
+    rozdil =  add(rozdil,expon(sub(i,prum),2))
+print(sqr(div(rozdil,sub(count,1)),2))
 
